d172d065b25e6a7a2de90b27110d6eee7cc18992
Jonas Bernoulli <jonas@bernoul.li>
2018-03-18 22:05:17 +0100

    Add element to mode-line-misc-info instead of mode-line-modes
    These variables are global and have nothing
    to do with a specific buffer and its modes.

--- mode-line-debug.el
+++ mode-line-debug.el
@@ -41,12 +41,10 @@ modes.  The inserted character can be used to toggle the state of
 `debug-on-error'."
   :global t
   :group 'mode-line
-  ;; We cannot insert `mode-line-debug' but have to insert it's value
-  ;; because we insert at the head of `mode-line-modes'. A list whose
-  ;; car is a symbol is interpreted as (CONDITION THEN ELSE).
-  (setq mode-line-modes (delete mode-line-debug mode-line-modes))
-  (when mode-line-debug-mode
-    (setq mode-line-modes (cons mode-line-debug mode-line-modes))))
+  (setq mode-line-misc-info
+        (if mode-line-debug-mode
+            (cons mode-line-debug mode-line-misc-info)
+          (delete mode-line-debug mode-line-misc-info))))
 
 (defcustom mode-line-debug-strings '("?" . " ")
   "Strings indicating the state of `debug-on-error' in the mode-line.
