4e9e4d774c69a694ff1c781ec2c9215b2f2dc643
Jonas Bernoulli <jonas@bernoul.li>
2022-04-22 18:15:52 +0200

    Refresh library header

--- mode-line-debug.el
+++ mode-line-debug.el
@@ -1,26 +1,27 @@
-;;; mode-line-debug.el --- show status of `debug-on-error' in the mode-line  -*- lexical-binding: t -*-
+;;; mode-line-debug.el --- Show status of debug-on-error in mode-line  -*- lexical-binding:t -*-
 
-;; Copyright (C) 2012-2022  Jonas Bernoulli
+;; Copyright (C) 2012-2022 Jonas Bernoulli
 
 ;; Author: Jonas Bernoulli <jonas@bernoul.li>
 ;; Homepage: https://github.com/tarsius/mode-line-debug
-;; Keywords: convenience, lisp
+;; Keywords: convenience lisp
+
 ;; Package-Requires: ((emacs "25.1") (compat "28.1.1.0"))
 
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 
-;; This file is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 3, or (at your option)
-;; any later version.
+;; This file is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published
+;; by the Free Software Foundation, either version 3 of the License,
+;; or (at your option) any later version.
 ;;
 ;; This file is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
 ;;
-;; For a full copy of the GNU General Public License
-;; see <http://www.gnu.org/licenses/>.
+;; You should have received a copy of the GNU General Public License
+;; along with this file.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
